70f5194a32cd96f3911e69429c795a420bbb5d2b
晏立 <yanli@yellfun.com>
2017-02-28 10:15:46 +0800

    test

---
+++ testMap.js
@@ -0,0 +1,14 @@
+/**
+ * Created by yan on 28/02/2017.
+ */
+seajs.use(['http://wx.indoorun.com/indoorun/app/yanli/indoorun/adk/modules/newMap', 'http://wx.indoorun.com/indoorun/app/yanli/indoorun/test/idrCoreManager'], function (idrmap, coreManager) {
+
+    coreManager.loadSessionSuccess = function () {
+
+        var map = new idrmap();
+
+        map.loadMap()
+    }
+
+    coreManager.init();
+});
