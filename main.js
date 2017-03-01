c1a01f6d108b8a67a5bc9a962bfcd00fe6aa9c43
晏立 <yanli@yellfun.com>
2017-03-01 09:20:45 +0800

    test

---
+++ main.js
@@ -0,0 +1,25 @@
+/**
+ * Created by yan on 27/02/2017.
+ */
+seajs.use([
+    'http://wx.indoorun.com/indoorun/app/yanli/indoorun/test/idrLocateServer',
+    'http://wx.indoorun.com/indoorun/app/yanli/indoorun/test/global',
+    'http://wx.indoorun.com/indoorun/app/yanli/indoorun/sdk/modules/loadmap'], function (
+        idrLocateServer,
+        gv,
+        idrMapView) {
+
+    gv.loadSessionSuccess = function () {
+
+        var locator = new idrLocateServer();
+
+        locator.start('14428254382730015', '14557583851000004', didLocateSuccess);
+
+        function didLocateSuccess(str) {
+
+            alert(str);
+        }
+    };
+
+    gv.init();
+});
