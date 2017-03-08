71119d939f7b1095e9c6f3281b1eb6e13dbd9f6a
晏立 <yanli@yellfun.com>
2017-03-08 14:19:36 +0800

    test

--- testTrans.js
+++ testTrans.js
@@ -3,7 +3,11 @@
  */
 seajs.use(['http://wx.indoorun.com/indoorun/app/yanli/indoorun/sdk/modules/matrix3', 'http://wx.indoorun.com/indoorun/app/yanli/indoorun/sdk/modules/math'], function (matrix3, xMath) {
 
-   var m = matrix3.create()
+    var m = matrix3.create()
+
+    console.log(m)
+
+    xMath.translate(m, 100, 100)
 
     console.log(m)
 });
