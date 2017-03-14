17abfede70749d6146873ed7db525aa24aa0be02
晏立 <yanli@yellfun.com>
2017-03-14 15:23:41 +0800

    寻路算法

--- testRoute.js
+++ testRoute.js
@@ -1,7 +1,7 @@
 /**
  * Created by yan on 14/03/2017.
  */
-seajs.use(['http://wx.indoorun.com/indoorun/app/yanli/indoorun/sdk/modules/pathRoute/RouteText'], function (RouteText) {
+seajs.use(['http://wx.indoorun.com/indoorun/app/yanli/indoorun/sdk/modules/pathRoute/RouteTest'], function (RouteText) {
 
 
     console.log('Ok')
