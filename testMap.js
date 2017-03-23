13a8caafbe7cbdd9ceedcaefc51ae830556574e4
晏立 <yanli@yellfun.com>
2017-03-23 16:26:05 +0800

    寻路算法

--- testMap.js
+++ testMap.js
@@ -7,11 +7,31 @@ seajs.use(['http://wx.indoorun.com/indoorun/app/yanli/indoorun/sdk/modules/idrMa
 
     var floorId = '14633645973301472'
 
+    function unzipBlob(blob, callback) {
+        // use a zip.BlobReader object to read zipped data stored into blob variable
+        zip.createReader(new zip.BlobReader(blob), function(zipReader) {
+            // get entries from the zip file
+            zipReader.getEntries(function(entries) {
+                // get data from the first file
+                entries[0].getData(new zip.BlobWriter("text/plain"), function(data) {
+                    // close the reader and calls callback function with uncompressed data as parameter
+                    zipReader.close();
+                    callback(data);
+                });
+            });
+        }, onerror);
+    }
+
     idrCoreMgr.loadSessionSuccess = function () {
 
         networkInstance.serverCallRegionPath('14633645971891335', function(data) {
 
-            console.log(data)
+            var decodedData = window.atob(data);
+
+            unzipBlob(decodedData, function(jsonData) {
+
+                console.log(jsonData)
+            })
 
         }, null)
 
